5c023ad57d1585df6a86c4145eba2bf890017f9c
Marcos Moura <marcos.work1998@gmail.com>
2021-06-09 18:16:38 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function Navblack()
+{
+	var nav = document.getElementById("navegacao");
+	nav.style.backgroundColor = "black";
+	link = document.getElementsByTagName("a")[0];
+	link.style.color = "white";
+	link = document.getElementsByTagName("a")[1];
+	link.style.color = "white";
+	link = document.getElementsByTagName("a")[2];
+	link.style.color = "white";
+	link = document.getElementsByTagName("a")[3];
+	link.style.color = "white";
+	link = document.getElementsByTagName("a")[4];
+	link.style.color = "white";
+	var box = document.getElementById("principal");
+	box.style.backgroundColor = "black";
+	var titlePrincipal = document.getElementsByTagName("h1")[0];
+	titlePrincipal.style.color = "#FFF";
+	var paragraph = document.getElementsByTagName("p")[0];
+	paragraph.style.color = "#FFF";
+	var paragraph = document.getElementsByTagName("p")[1];
+	paragraph.style.color = "#FFF";
+	var link_principal = document.getElementById("link_principal");
+	link_principal.style.color = "#FFF";
+	link_principal.style.border = "2px solid #FFF"
+
+}
+
+function Navwhite()
+{
+	var nav = document.getElementById("navegacao");
+	nav.style.backgroundColor = "white"
+	link = document.getElementsByTagName("a")[0]
+	link.style.color = "#1351b4"
+	link = document.getElementsByTagName("a")[1]
+	link.style.color = "#1351b4"
+	link = document.getElementsByTagName("a")[2]
+	link.style.color = "#1351b4"
+	link = document.getElementsByTagName("a")[3]
+	link.style.color = "#1351b4"
+	link = document.getElementsByTagName("a")[4]
+	link.style.color = "#1351b4"
+	var box = document.getElementById("principal");
+	box.style.backgroundColor = "#FFF";
+	var titlePrincipal = document.getElementsByTagName("h1")[0];
+	titlePrincipal.style.color = "#1351b4";
+	var paragraph = document.getElementsByTagName("p")[0];
+	paragraph.style.color = "#1351b4";
+	var paragraph = document.getElementsByTagName("p")[1];
+	paragraph.style.color = "#1351b4";
+	var link_principal = document.getElementById("link_principal");
+	link_principal.style.color = "#1351b4";
+	link_principal.style.border = "2px solid #1351b4"
+}
